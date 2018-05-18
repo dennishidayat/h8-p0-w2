0cdc2a4bc12e071aeeae93b2de26891c18d2fa24
dennishidayat <37436447+dennishidayat@users.noreply.github.com>
2018-05-18 13:32:22 +0700

    Update WEEK2_exercise4.js

--- WEEK2_exercise4.js
+++ WEEK2_exercise4.js
@@ -1,24 +1,20 @@
-var tanggal;
-var bulan;
-var tahun;
+var tanggal = 19;
+var bulan = 3;
+var tahun = 2000;
 
-function kalender(tanggal, bulan, tahun){
-
-  switch(bulan) {
-    case 1: {bulan = "januari"}
-    case 2: {bulan = "februari"}
-    case 3: {bulan = "maret"}
-    case 4: {bulan = "april"}
-    case 5: {bulan = "mei"}
-    case 6: {bulan = "juni"}
-    case 7: {bulan = "juli"}
-    case 8: {bulan = "agustus"}
-    case 9: {bulan = "september"}
-    case 10: {bulan = "oktober"}
-    case 11: {bulan = "november"}
-    case 12: {bulan = "desember"}
-  }
-  console.log(tanggal,  bulan, tahun);
+switch(bulan) {
+  case 1: {bulan = "januari"}
+  case 2: {bulan = "februari"}
+  case 3: {bulan = "maret"}
+  case 4: {bulan = "april"}
+  case 5: {bulan = "mei"}
+  case 6: {bulan = "juni"}
+  case 7: {bulan = "juli"}
+  case 8: {bulan = "agustus"}
+  case 9: {bulan = "september"}
+  case 10: {bulan = "oktober"}
+  case 11: {bulan = "november"}
+  case 12: {bulan = "desember"}
 }
 
-kalender(1, 1, 2000);
+console.log(tanggal + " " + bulan + " " + tahun);
