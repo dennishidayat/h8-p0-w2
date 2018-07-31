5832ad12dd1ae33af411629f2fcb92a4bcf8b300
dennishidayat <37436447+dennishidayat@users.noreply.github.com>
2018-08-01 06:08:28 +0700

    Update WEEK2_exercise3.js

--- WEEK2_exercise3.js
+++ WEEK2_exercise3.js
@@ -1,24 +1,22 @@
-var nama;
-nama = null;
-var peran;
-peran = null;
+var nama = 'Mikael';
+var peran = 'tabib';
 
-function game(nama, peran){
   if (nama == null, peran == null){
   console.log("Nama harus diisi!");
   }
-  if (nama !== null, peran == null){
+  else if (nama !== null, peran == null){
     console.log("Halo ", nama, ", Pilih peranmu untuk memulai game!");
   }
-  if (nama !== null , peran == "ksatria"){
+  else if (nama !== null , peran == "ksatria"){
+    console.log("Selamat datang di Dunia Proxytia, ", nama);
     console.log("Halo Ksatria", nama, ", kamu dapat menyerang dengan senjatamu!");
   }
-  if (nama !== null , peran == "tabib"){
+  else if (nama !== null , peran == "tabib"){
+    console.log("Selamat datang di Dunia Proxytia, ", nama);
     console.log("Halo Tabib ", nama, ", kamu akan membantu temanmu yang terluka!");
   }
-  if (nama !== null , peran == "penyihir"){
+  else if (nama !== null , peran == "penyihir"){
+    console.log("Selamat datang di Dunia Proxytia, ", nama);
     console.log("Halo Penyihir ", nama, ", ciptakan keajaiban yang membantu kemenanganmu!");
   }
-}
 
-game("dennis","tabib");
